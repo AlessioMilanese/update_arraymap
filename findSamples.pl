2a69a4958d32e1cbc4a1b4a0ef12c9fef2f6ed48
AlessioMilanese <alessio@IMLS-NBM-ALMI.local>
2016-01-18 15:56:53 +0100

    solve problem of selecting the right samples

--- findSamples.pl
+++ findSamples.pl
@@ -47,33 +47,20 @@ my $mongosamples = pgGetMongoCursor(
 
 print "download all the platforms...";
 my %arraymap_platforms = map{ $_->{ PLATFORMID } => 1 }	(grep{ $_->{ PLATFORMID } =~ /GPL/ } @{ $mongosamples });
-
-
-
-my @keyss = keys %arraymap_platforms;
-	for my $samplee (@keyss){
-		print "The colorr of '$samplee'\n";
-		print "i'$samplee'\n";
-	}
-
-my @fruits = keys %arraymap_platforms;
-	for my $fruit (@fruits) {
-			print "The color of '$fruit'\n";
-	}
-
-$size = @keyss;
+my @arraymap_platforms = keys %arraymap_platforms;
+$size = @arraymap_platforms;
 print "done: $size platforms found. \n";
 
 print "download all the series...";
 my %arraymap_series = map{ $_->{ SERIESID } => 1 } (grep{ $_->{ SERIESID } =~ /GSE/ } @{ $mongosamples });
-@keys = keys %arraymap_series;
-$size = @keys;
+@arraymap_series = keys %arraymap_series;
+$size = @arraymap_series;
 print "done: $size series found. \n";
 
 print "download all the samples...";
 my %arraymap_samples = map{ $_->{ UID } => 1 } (grep{ $_->{ UID } =~ /GSM/ } @{ $mongosamples });
-@keys = keys %arraymap_samples;
-$size = @keys;
+@arraymap_samples = keys %arraymap_samples;
+$size = @arraymap_samples;
 print "done: $size samples found. \n\n";
 
 
@@ -119,21 +106,18 @@ my $filename = "new_samples.txt";
 open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
 
 foreach my $plat (sort keys %arraymap_platforms){
-
 	my $address            =     "http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=$plat&targ=self&view=brief&form=text";
 	my $GSMlist            =     [ grep{/GSM\d+/} split(/[\n\r]/,get($address))];
 
 	foreach my $sample (@$GSMlist){
 	    $GSM = substr("$sample",22);
 			my $match = 0;
-			$match = first { /"$GSM"/ } %arraymap_samples;
-			print "$plat $GSM $match ";
-			if ($match == 0){
+			@result = grep /$GSM/, @arraymap_samples;
+			$res = @result[0];
+			if ($res eq $GSM){
+			}else{
 				print $fh "$plat $GSM\n";
 				$n_sample = $n_sample + 1;
-				print "enter\n";
-			}else{
-				print "\n";
 			}
 	}
 
