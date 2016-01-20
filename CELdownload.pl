05ae0ffcd54b98199ec4c7f191e14d3929433653
AlessioMilanese <alessio@IMLS-NBM-ALMI.connectify>
2016-01-20 10:50:26 +0100

    improvement of the code and comments

--- CELdownload.pl
+++ CELdownload.pl
@@ -1,21 +1,9 @@
-# Script that downloads all the samples that are not in Arraymap.
-# In order to do that we download all the samples, series and samples that are
-# already in Arraymap. After that, we download all the samples related to the
-# platform that are in Arraymap and we check which of those are not in Arraymap.
+# Script that downloads a .txt file
 
 #!/usr/bin/perl
 
-use lib qw(/Library/WebServer/cgi-bin);
-use PG;
 use File::Fetch;
-use Net::FTP;
-use Archive::Tar;
-use IO::Handle;
-STDOUT->autoflush(1);
 
-
-
-use File::Fetch;
 ### build a File::Fetch object ###
 my $ff = File::Fetch->new(uri => 'http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GPL8355&targ=self&view=brief&form=text');
 ### fetch the uri to cwd() ###
