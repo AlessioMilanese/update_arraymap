b4a43f4f3668bd5c6cd14e59453a91fef591f1d3
Progenetix - genomic aberrations in cancer <contact@progenetix.org>
2016-01-27 12:43:59 +0100

    celfile_review
    * modified/extended version of the celfile counter, including input
    parameters, array map filtering etc.

---
+++ celfile_review.pl
@@ -0,0 +1,106 @@
+# Script that counts the number of .CEL files of the samples that we need to download
+
+#!/usr/bin/perl
+use File::Fetch;
+use Archive::Extract;
+use lib qw(/Library/WebServer/cgi-bin);
+use PG;
+use LWP::Simple;
+STDOUT->autoflush(1);
+
+# platform that we are going to analyze
+# platforms: "GPL6801", "GPL2641", "GPL3718", "GPL3720", "GPL2004", "GPL2005", "GPL1266"
+
+my %args              =   @ARGV;
+
+$args{ LOC_USERID }   =   getlogin();
+$args{ LOC_ROOT }     =   '/Library/WebServer/Documents';
+
+$args{ '-dataroot' }  //=	'/Volumes/arrayRAID/arraymapIn/GEOupdate';
+$args{ '-metaroot' }  //=	'/Volumes/arrayRAID/arraymapIn/GEOmeta';
+$args{ '-arraymap' }  //= 'n';
+$args{ '-getmeta' }   //= 'n';
+$args{ '-randno' }    //= -1;
+$args{ '-gpl' }       //= "GPL6801,GPL2641,GPL3718,GPL3720,GPL2004,GPL2005,GPL1266";
+
+$args{pgP}            =   pgSetPaths(%args);
+$args{pgV}            =   setValueDefaults();
+%args                 =	  pgModifyArgs(%args);
+
+################################################################################
+
+my @celPlatforms      =   split(',', $args{ '-gpl' });
+
+my @n_samples;
+
+print "start\n";
+
+my $gsmIndexFile      =   $args{ '-dataroot' }."/gsmdata.tab";
+my @gsmIndex          =   @{ pgFile2list($gsmIndexFile) };
+my @gsmInfoKeys       =   split("\t", shift(@gsmIndex));
+my %gsmInfo;
+
+my $arraymapGPL       =   [];
+
+if ($args{ '-arraymap' } !~ /y/) {
+
+  _d('looking up arraymap for existing samples');
+
+  $arraymapGSM		    =	  pgGetMongoDistinct(
+                            %args,
+                            MDBCOLL		=>	'samples',
+                            MDB       =>  'arraymap',
+                            KEY				=>	'UID',
+                            QUERY			=>	{},
+                          );
+
+  _d('retrieved', scalar @{ $arraymapGSM },'arraymap IDs for filtering');
+
+}
+
+foreach (@gsmIndex) {
+
+  my @currentGSM      =   split("\t", $_);
+  my $currentGSMmap   =   { map{ $gsmInfoKeys[$_] => $currentGSM[$_] } 0..$#gsmInfoKeys };
+
+  if (
+    (any { $currentGSMmap->{GPL} eq $_ } @celPlatforms)
+    &&
+    (! any { $currentGSMmap->{GSM} eq $_ } @{ $arraymapGSM })
+  ) {
+
+    $gsmInfo{ $currentGSMmap->{GSM} } =   $currentGSMmap;
+
+}}
+
+_d(scalar keys %gsmInfo, 'GSM have been found for any of', @celPlatforms);
+
+foreach my $gsm (sort keys %gsmInfo) {
+
+  $gsmInfo{ $gsm }->{CELFILEFTP}  =   [];
+
+  open my $ff, '<:encoding(UTF-8)', $gsmInfo{ $gsm }->{FILE} or die;
+
+  foreach my $ftpCELfile (grep{ /Sample_supplementary_file = ftp.*?\.CEL\.gz.*?/ } <$ff>) {
+
+    chomp $ftpCELfile;
+    $ftpCELfile       =~  s/^.*?\= (ftp.*?\.CEL\.gz).*?$/$1/;
+    push(@{ $gsmInfo{ $gsm }->{CELFILEFTP} }, $ftpCELfile);
+
+  }
+
+  _d($gsm, scalar @{$gsmInfo{ $gsm }->{CELFILEFTP}}, 'CEL files');
+
+}
+
+my $celInfoFile       =   $gsmIndexFile;
+$celInfoFile          =~  s/\.\w\w\w$/_celfiles.tab/;
+
+pgWriteFile(
+  FILE					    =>	$celInfoFile,
+  CONTENT					  =>	join("\n", map{ join("\t", ($_, $gsmInfo{ $_ }->{GPL}, scalar @{ $gsmInfo{ $_ }->{CELFILEFTP} }, $gsmInfo{ $_ }->{CELFILEFTP}->[0])) } keys %gsmInfo),
+);
+
+_d(scalar(grep{ scalar @{ $gsmInfo{ $_ }->{CELFILEFTP} } == 1 } keys %gsmInfo), 'of', scalar keys %gsmInfo, 'GSM had 1 CEL file');
+_d(scalar(grep{ scalar @{ $gsmInfo{ $_ }->{CELFILEFTP} } > 1 } keys %gsmInfo), 'of', scalar keys %gsmInfo, 'GSM had more than 1 CEL file');
+_d(scalar(grep{ scalar @{ $gsmInfo{ $_ }->{CELFILEFTP} } < 1 } keys %gsmInfo), 'of', scalar keys %gsmInfo, 'GSM had no CEL file');
